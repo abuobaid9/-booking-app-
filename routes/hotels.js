e13cf6239505ae0a3ccb4938b1864d45d2c4894d
abuobaid9 <abuobaid.anas1999@gmail.com>
2023-01-31 22:23:11 +0000

    done

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,31 +1,33 @@
-import express from 'express'
-import { countByCity, countByType, createHotel, deleteHotel, getHotel, getHotels, getHotleRoom, updateHotel } from '../controllers/hotel.js';
-import { verifyAdmin } from '../utils/verifyToken.js';
-
+import express from "express";
+import {
+  countByCity,
+  countByType,
+  createHotel,
+  deleteHotel,
+  getHotel,
+  getHotelRooms,
+  getHotels,
+  updateHotel,
+} from "../controllers/hotel.js";
+import Hotel from "../models/Hotel.js";
+import {verifyAdmin} from "../utils/verifyToken.js"
 const router = express.Router();
 
+//CREATE
+router.post("/", verifyAdmin, createHotel);
 
-//create
-router.post('/',verifyAdmin, createHotel)
+//UPDATE
+router.put("/:id", verifyAdmin, updateHotel);
+//DELETE
+router.delete("/:id", verifyAdmin, deleteHotel);
+//GET
 
+router.get("/find/:id", getHotel);
+//GET ALL
 
-//update
-router.put('/find/:id',verifyAdmin, updateHotel)
-
-
-//delete
-router.delete('/:id',verifyAdmin, deleteHotel)
-
-
-//getOne
-router.get('/find/:id', getHotel)
-
-
-//getAll
-router.get('/', getHotels)
-
-router.get('/countByCity', countByCity)
-router.get('/countByType', countByType)
-router.get('/room/:id', getHotleRoom)
+router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
+router.get("/room/:id", getHotelRooms);
 
 export default router;
